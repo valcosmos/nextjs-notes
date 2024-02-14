6d5fcac557f307d82710680760a2f41869c6dc91
Cupid Valentine <westerngod@126.com>
2024-02-14 11:13:36 +0800

    feat: setup strapi

---
+++ apps/nextjs-notes-strapi/src/admin/webpack.config.example.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = (config, _) => {
+  // Note: we provide webpack above so you should not `require` it
+  // Perform customizations to webpack config
+  // Important: return the modified config
+  return config
+}
